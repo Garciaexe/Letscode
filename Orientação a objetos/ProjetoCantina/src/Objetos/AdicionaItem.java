3f07aa09b442af1a43d7dd31cc33e7975bc2d056
Gabriel Garcia <gabrielgarciagalvao@gmail.com>
2022-03-13 19:01:04 -0300

    Projeto cantina

--- Orientação a objetos/ProjetoCantina/src/Objetos/AdicionaItem.java
+++ Orientação a objetos/ProjetoCantina/src/Objetos/AdicionaItem.java
@@ -8,13 +8,15 @@ public class AdicionaItem extends TaxaEntrega{
     ArrayList<Integer> quantidadeItem = new ArrayList<>();
     int contador;
     int total;
+    int quantidadeDefault;
 
     public void adicionaItem(String item, int quantidade, int valor, int tipoTaxa){
-        itens.add(item);
-        quantidadeItem.add(quantidade);
+        this.itens.add(item);
+        this.quantidadeItem.add(quantidade);
+        this.quantidadeDefault = quantidade;
         quantidade = valor * quantidade;
-        valores.add(quantidade);
-        taxaEntrega(tipoTaxa, quantidade);
-        contador += 1;
+        this.valores.add(quantidade);
+        taxaEntrega(tipoTaxa, this.quantidadeDefault);
+        this.contador += 1;
     }
 }
