5b80574ae01fbe04660c0a130b4c8bce61059555
Gabriel Garcia <gabrielgarciagalvao@gmail.com>
2022-03-13 20:44:22 -0300

    Projeto cantina

--- Orientação a objetos/ProjetoCantina/src/Objetos/AdicionaItem.java
+++ Orientação a objetos/ProjetoCantina/src/Objetos/AdicionaItem.java
@@ -1,10 +1,11 @@
 package Objetos;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class AdicionaItem extends TaxaEntrega {
     ArrayList<String> itens = new ArrayList<>();
-    ArrayList<Integer> valores = new ArrayList<>();
+    ArrayList<Double> valores = new ArrayList<>();
     ArrayList<Integer> quantidadeItem = new ArrayList<>();
     private int contador;
 
@@ -12,14 +13,25 @@ public class AdicionaItem extends TaxaEntrega {
         return contador;
     }
 
-
-    public void adicionaItem(String item, int quantidade, int valor, int tipoTaxa) {
-                this.itens.add(item);
-                this.quantidadeItem.add(quantidade);
-                this.valores.add(quantidade * valor);
+    public void adicionaItem(String item, int quantidade, Double valor, int tipoTaxa) {
+        int validaItemJaPedido = 1;
+        Double valorTotal;
+        for(int i = 0; i < this.contador; i++){
+            if(Objects.equals(this.itens.get(i), item)){
+                valorTotal = valor * quantidade;
+                this.valores.set(i, this.valores.get(i) + valorTotal   );
+                this.quantidadeItem.set(i, this.quantidadeItem.get(i) + quantidade);
                 taxaEntrega(tipoTaxa, quantidade);
-                this.contador += 1;
-
+                validaItemJaPedido = 2;
+            }
+        }
+        if(validaItemJaPedido == 1) {
+            this.itens.add(item);
+            this.quantidadeItem.add(quantidade);
+            this.valores.add(quantidade * valor);
+            taxaEntrega(tipoTaxa, quantidade);
+            this.contador += 1;
+        }
     }
 }
 
--- Orientação a objetos/ProjetoCantina/src/Objetos/SelecionadorDePratos.java
+++ Orientação a objetos/ProjetoCantina/src/Objetos/SelecionadorDePratos.java
@@ -6,44 +6,41 @@ import java.util.Scanner;
 
 public class SelecionadorDePratos extends FechaPedido {
     Scanner scan = new Scanner(System.in);
-    private String item;
-    private int quantidade;
-    private int valor;
 
     //Selecionador de pratos
     public void Selecionador() throws ItemInexistenteException {
         System.out.println("\nDigite o nome do item desejado:");
-        this.item = scan.nextLine().toLowerCase();
+        String item = scan.nextLine().toLowerCase();
         System.out.println("Digite a quantidade desejada:");
-        this.quantidade = scan.nextInt();
+        int quantidade = scan.nextInt();
         scan.nextLine();
-        switch(this.item){
+        switch(item){
             case "burguer":
-                adicionaItem(this.item, this.quantidade, 30, 1);
+                adicionaItem(item, quantidade, 30.00, 1);
                 break;
             case "pizza":
-                adicionaItem(this.item, this.quantidade, 45, 1);
+                adicionaItem(item, quantidade, 45.00, 1);
                 break;
             case "fogazza":
-                adicionaItem(this.item, this.quantidade, 20, 1);
+                adicionaItem(item, quantidade, 20.00, 1);
                 break;
             case "pasta":
-                adicionaItem(this.item, this.quantidade, 30, 2);
+                adicionaItem(item, quantidade, 30.00, 2);
                 break;
             case "lasanha":
-                adicionaItem(this.item, this.quantidade, 70, 2);
+                adicionaItem(item, quantidade, 70.00, 2);
                 break;
             case "polpetone":
-                adicionaItem(this.item, this.quantidade, 50, 2);
+                adicionaItem(item, quantidade, 50.00, 2);
                 break;
             case "refrigerante":
-                adicionaItem(this.item, this.quantidade, 5, 1);
+                adicionaItem(item, quantidade, 5.00, 1);
                 break;
             case "suco":
-                adicionaItem(this.item, this.quantidade, 7, 1);
+                adicionaItem(item, quantidade, 7.00, 1);
                 break;
             case "vinho":
-                adicionaItem(this.item, this.quantidade, 50, 1);
+                adicionaItem(item, quantidade, 50.00, 1);
                 break;
             default: throw new ItemInexistenteException("Item não encontrado");
         }
