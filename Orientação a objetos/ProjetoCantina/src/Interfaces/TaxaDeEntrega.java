7dfc801c6a6f4a59acfb43ce8a1ec117a05fad58
Gabriel Garcia <gabrielgarciagalvao@gmail.com>
2022-03-14 10:50:54 -0300

    Projeto cantina

--- Orientação a objetos/ProjetoCantina/src/Interfaces/TaxaDeEntrega.java
+++ Orientação a objetos/ProjetoCantina/src/Interfaces/TaxaDeEntrega.java
@@ -1,5 +1,5 @@
 package Interfaces;
 
 public interface TaxaDeEntrega {
-    public void taxaEntrega(int tipotaxa, int quantidade);
+    public void taxaEntrega(int tipoTaxa, int quantidade);
 }
--- Orientação a objetos/ProjetoCantina/src/Objetos/TaxaEntrega.java
+++ Orientação a objetos/ProjetoCantina/src/Objetos/TaxaEntrega.java
@@ -4,13 +4,21 @@ import Interfaces.TaxaDeEntrega;
 
 public class TaxaEntrega implements TaxaDeEntrega {
     private int taxa;
+    private final int tipoTaxaUm = 1;
+    private final int tipoTaxaDois = 2;
 
     public int getTaxa() {
         return taxa;
     }
 
     @Override
-    public void taxaEntrega(int tipotaxa, int quantidade) {
-            this.taxa += tipotaxa * quantidade;
+    public void taxaEntrega(int tipoTaxa, int quantidade) {
+            if(tipoTaxa == 1){
+                this.taxa += this.tipoTaxaUm * quantidade;
+            }else if(tipoTaxa == 2){
+                this.taxa += this.tipoTaxaDois * quantidade;
+        }else{
+                System.out.println("Tipo de taxa inválido");
+            }
     }
 }
